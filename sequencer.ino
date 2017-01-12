38b623194b9c345a62a638744d7626f9eb33d8af
jamjambles <z3461612@ad.unsw.edu.au>
2017-01-12 13:34:08 +1100

    Add files via upload

--- sequencer.ino
+++ sequencer.ino
@@ -1,8 +1,21 @@
+//runs on the atmega2560
+
 //sequence specs
 #define NUMBER_OF_STEPS_PER_BEAT 4
-#define NUMBER_OF_BEATS 16
+#define NUMBER_OF_BARS 1
+#define NUMBER_OF_BEATS_PER_BAR 4
+#define NUMBER_OF_BEATS (NUMBER_OF_BARS*NUMBER_OF_BEATS_PER_BAR)
 #define NUMBER_OF_DRUMS 7
-#define NUMBER_OF_STEPS (NUMBER_OF_STEPS_PER_BEAT*NUMBER_OF_BEATS)
+#define REST 0
+#define NUMBER_OF_STEPS (NUMBER_OF_STEPS_PER_BEAT*NUMBER_OF_BEATS+REST)
+#define SEQUENCE_LENGTH (NUMBER_OF_STEPS*NUMBER_OF_DRUMS)
+
+//if sequence length is too long, store it in flash mem
+//3000 is somewhat arbitrary
+#if SEQUENCE_LENGTH > 3000
+  #include <avr/pgmspace.h> 
+  #define PROGMEM_SET
+#endif
 
 //sequence order
 #define SNARE_OFFSET  0
@@ -13,14 +26,19 @@
 #define RIDE_OFFSET   5
 #define FTOM_OFFSET   6
 
-//hit strengths
+//hit strengths. Provide ability to accent notes
 #define WELL    255
-#define MED     180
+#define MED     230
 #define RARE    200
 #define NO_HIT  0
 
 //interupt pins
-const byte PULSE_IN = 19;
+const byte PULSE_IN = 19; // The beats from the algo
+const byte MUTE_IN = 20; // Will mute/unmute the drums
+const byte PHASE_RST_IN = 21; // Correct any phase issues during performance
+
+//count in
+#define NUMBER_OF_COUNT_IN_BARS 1
 
 //drum pins
 #define SNARE 2
@@ -39,13 +57,13 @@ const byte PULSE_IN = 19;
 #define TIMER_COUNTS  ((TIMER_TIME*10^-3 / TIMER_RES) -1)
 
 //drum times
-#define KICK_TIME   130 //ms
+#define KICK_TIME   90 //ms
 #define SNARE_TIME  40  //ms  
 #define HAT_TIME    40  //ms
 #define CRASH_TIME  80  //ms
 #define TOM1_TIME   40
-#define RIDE_TIME   100
-#define FTOM_TIME   50
+#define RIDE_TIME   40
+#define FTOM_TIME   40
 
 //interupt stuff
 volatile bool kick_active;
@@ -56,7 +74,6 @@ volatile bool tom1_active;
 volatile bool ride_active;
 volatile bool ftom_active;
 
-
 volatile int s_multiple_of_5;
 volatile int k_multiple_of_5;
 volatile int h_multiple_of_5;
@@ -66,252 +83,38 @@ volatile int r_multiple_of_5;
 volatile int ft_multiple_of_5;
 
 volatile int seq_count;
-/*
-volatile int sequence[NUMBER_OF_STEPS*NUMBER_OF_DRUMS] = 
+volatile int count_in;
+volatile bool mute_flag;
+volatile int global_count;
+
+#ifdef PROGMEM_SET
+const PROGMEM unsigned int sequence[NUMBER_OF_STEPS*NUMBER_OF_DRUMS]  = 
+#else
+const unsigned int sequence[(NUMBER_OF_STEPS)*NUMBER_OF_DRUMS]  = 
+#endif
 {
-//snare, kick, hat, crash, tom1, ride, ftom
-  NO_HIT, WELL, WELL, WELL, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 1
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
+//Order of drums: snare, kick, hat, crash, tom1, ride, floor tom//
+NO_HIT,WELL,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,NO_HIT,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,WELL,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,NO_HIT,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+
+NO_HIT,WELL,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,NO_HIT,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,WELL,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,NO_HIT,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+
+NO_HIT,WELL,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,NO_HIT,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,WELL,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,NO_HIT,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+
+NO_HIT,WELL,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,NO_HIT,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,WELL,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
+NO_HIT,NO_HIT,WELL,NO_HIT,NO_HIT,NO_HIT,NO_HIT,
 
-  WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 2
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
 
-  NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 4
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 1
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 2
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 4
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 1
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 2
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 4
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-  
-  WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,     //step 1  `//beat 1
-  WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 2
-  WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,    //step 3
-  WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT,  //step 4
-
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT,     //step 1  `//beat 2
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT,  //step 2
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT,  //step 4
-
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL,     //step 1  `//beat 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL,  //step 2
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL,    //step 3
-  NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL,  //step 4
-  
-};
-*/
-
-volatile int sequence[NUMBER_OF_STEPS*NUMBER_OF_DRUMS] = 
-{
-//snare, kick, hat, crash, tom1, ride, ftom
-//bar 1, beat 1
-NO_HIT, WELL, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 2
-WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 3
-NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 4
-WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//bar 2 beat 1
-NO_HIT, WELL, WELL, NO_HIT, NO_HIT, WELL, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, WELL, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 2
-WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 3
-NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 4
-WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-//bar 3 beat 1
-NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 2
-WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 3
-NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 4
-WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-//bar 4 beat 1
-NO_HIT, WELL, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 2
-WELL, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-WELL, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, 
-
-
-//beat 3
-WELL, NO_HIT, WELL, NO_HIT, WELL, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, 
-
-
-//beat 4
-NO_HIT, NO_HIT, WELL, NO_HIT, NO_HIT, NO_HIT, WELL, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, 
-
-NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, NO_HIT, WELL, 
 };
 //pwm stuff
 int eraser = 7;
@@ -331,9 +134,13 @@ void setup() {
   TCCR1A = 0; //Timer 1 (used by servo lib)
   TCCR1B = 0;
   interrupts();            
-  
-  attachInterrupt(digitalPinToInterrupt(PULSE_IN),write_drums_high,RISING); //Whenever pin 20 goes from low to high write drums
 
+  attachInterrupt(digitalPinToInterrupt(MUTE_IN),mute,HIGH);
+  attachInterrupt(digitalPinToInterrupt(MUTE_IN),unmute,LOW);
+  attachInterrupt(digitalPinToInterrupt(PULSE_IN),write_drums_high,RISING); //Whenever pin 19 goes from low to high write drums
+ 
+  //attachInterrupt(digitalPinToInterrupt(PHASE_RST_IN),phase_rst,RISING);
+  
   kick_active = false;
   snare_active = false;
   hat_active = false;
@@ -342,6 +149,9 @@ void setup() {
   ride_active = false;
   ftom_active = false;
 
+  count_in = NUMBER_OF_COUNT_IN_BARS*NUMBER_OF_BEATS_PER_BAR*NUMBER_OF_STEPS_PER_BEAT;
+  mute_flag = false;
+  
   s_multiple_of_5 = 0;
   k_multiple_of_5 = 0;
   h_multiple_of_5 = 0;
@@ -349,73 +159,157 @@ void setup() {
   t1_multiple_of_5 = 0;
   r_multiple_of_5 = 0;
   ft_multiple_of_5 = 0;
+  
+  //setting pwm frequency to 31KHz on pins 2,3,5,6,7,8,9,10
+  set_pwm_2_3_5_6_7_8_9_10(); 
 
-  set_pwm_2_3_5_6_7_8_9_10(); //setting pwm frequency to 31KHz
+  global_count = 0;
 }
 
 void loop() {
 
 }
 
+void mute(){
+  mute_flag = true;
+}
+
+void unmute(){
+  seq_count = global_count%4;
+  
+  mute_flag = false;
+}
+
+void phase_rst(){
+  seq_count = 0; 
+  
+}
+
+
 void write_drums_high()
 {
+  global_count++;
+  //if(mute_flag == false && count_in == 0)
+  if(mute_flag == false)
+  { 
+    bool is_hit = false;
+    
+#ifdef PROGMEM_SET
+   
+    if(snare_active==false&&pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + SNARE_OFFSET) != NO_HIT)
+    {     
+      analogWrite(SNARE,pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + SNARE_OFFSET));   //snare
+      snare_active = true;
+      is_hit = true;
+    }
+    
+    
+    if(kick_active==false&&pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + KICK_OFFSET) != NO_HIT)
+    {
+      analogWrite(KICK,pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + KICK_OFFSET)); //kick
+      kick_active = true;
+      is_hit = true;
+    }
+    
+    if(hat_active==false&&pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + HAT_OFFSET) != NO_HIT)
+    {
+      analogWrite(HAT,pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + HAT_OFFSET));
+      hat_active = true;
+      is_hit = true;
+    }
+    
+    if(crash_active==false&&pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + CRASH_OFFSET) != NO_HIT)
+    {
+      analogWrite(CRASH,pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + CRASH_OFFSET));
+      crash_active = true;
+      is_hit = true;
+    }
+    
+    if(tom1_active==false&&pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + TOM1_OFFSET) != NO_HIT)
+    {
+      analogWrite(TOM1,pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + TOM1_OFFSET));
+      tom1_active = true;
+      is_hit = true;
+    }
 
-  bool is_hit = false;
-
-  if(snare_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + SNARE_OFFSET] != NO_HIT)
-  {
-    analogWrite(SNARE,sequence[seq_count*NUMBER_OF_DRUMS + SNARE_OFFSET]);   //snare
-    snare_active = true;
-    is_hit = true;
+    if(ride_active==false&&pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + RIDE_OFFSET) != NO_HIT)
+    {
+      analogWrite(RIDE,pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + RIDE_OFFSET));
+      ride_active = true;
+      is_hit = true;
+    }
+    
+    if(ftom_active==false&&pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + FTOM_OFFSET) != NO_HIT)
+    {
+      analogWrite(FTOM,pgm_read_word_near(sequence+seq_count*NUMBER_OF_DRUMS + FTOM_OFFSET));
+      ftom_active = true;
+      is_hit = true;
+    }
+#else
+    if(snare_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + SNARE_OFFSET] != NO_HIT)
+    {
+      analogWrite(SNARE,sequence[seq_count*NUMBER_OF_DRUMS + SNARE_OFFSET]);   //snare
+      snare_active = true;
+      is_hit = true;
+    }
+    
+    if(kick_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + KICK_OFFSET] != NO_HIT)
+    {
+      analogWrite(KICK,sequence[seq_count*NUMBER_OF_DRUMS + KICK_OFFSET]);  //kick
+      kick_active = true;
+      is_hit = true;
+    }
+    
+    if(hat_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + HAT_OFFSET] != NO_HIT) 
+    {
+      analogWrite(HAT,sequence[seq_count*NUMBER_OF_DRUMS + HAT_OFFSET]);  //hat
+      hat_active = true;
+      is_hit = true;
+    }
+    
+    if(crash_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + CRASH_OFFSET] != NO_HIT) 
+    {
+      analogWrite(CRASH,sequence[seq_count*NUMBER_OF_DRUMS + CRASH_OFFSET]);  //crash
+      crash_active = true;
+      is_hit = true;
+    }
+    
+    if(tom1_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + TOM1_OFFSET] != NO_HIT) 
+    {
+      analogWrite(TOM1,sequence[seq_count*NUMBER_OF_DRUMS + TOM1_OFFSET]);  //tom1
+      tom1_active = true;
+      is_hit = true;
+    }
+    
+    if(ride_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + RIDE_OFFSET] != NO_HIT) 
+    {
+      analogWrite(RIDE,sequence[seq_count*NUMBER_OF_DRUMS + RIDE_OFFSET]);  //ride
+      ride_active = true;
+      is_hit = true;
+    }
+    
+    if(ftom_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + FTOM_OFFSET] != NO_HIT) 
+    {
+      analogWrite(FTOM,sequence[seq_count*NUMBER_OF_DRUMS + FTOM_OFFSET]);  //ftom
+      ftom_active = true;
+      is_hit = true;
+    }
+#endif
+    if (is_hit)
+      begin_5_timer();
+         
+    seq_count++;
+    seq_count = seq_count % NUMBER_OF_STEPS;
   }
-
-  if(kick_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + KICK_OFFSET] != NO_HIT)
+  else
   {
-    analogWrite(KICK,sequence[seq_count*NUMBER_OF_DRUMS + KICK_OFFSET]);  //kick
-    kick_active = true;
-    is_hit = true;
+    
   }
   
- if(hat_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + HAT_OFFSET] != NO_HIT) 
-  {
-    analogWrite(HAT,sequence[seq_count*NUMBER_OF_DRUMS + HAT_OFFSET]);  //hat
-    hat_active = true;
-    is_hit = true;
-  }
-
-  if(crash_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + CRASH_OFFSET] != NO_HIT) 
-  {
-    analogWrite(CRASH,sequence[seq_count*NUMBER_OF_DRUMS + CRASH_OFFSET]);  //crash
-    crash_active = true;
-    is_hit = true;
-  }
-
-  if(tom1_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + TOM1_OFFSET] != NO_HIT) 
-  {
-    analogWrite(TOM1,sequence[seq_count*NUMBER_OF_DRUMS + TOM1_OFFSET]);  //crash
-    tom1_active = true;
-    is_hit = true;
-  }
-
-  if(ride_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + RIDE_OFFSET] != NO_HIT) 
-  {
-    analogWrite(RIDE,sequence[seq_count*NUMBER_OF_DRUMS + RIDE_OFFSET]);  //crash
-    ride_active = true;
-    is_hit = true;
-  }
-
-  if(ftom_active==false&&sequence[seq_count*NUMBER_OF_DRUMS + FTOM_OFFSET] != NO_HIT) 
-  {
-    analogWrite(FTOM,sequence[seq_count*NUMBER_OF_DRUMS + FTOM_OFFSET]);  //crash
-    ftom_active = true;
-    is_hit = true;
-  }
-        
-  if (is_hit)
-    begin_5_timer();
-       
-  seq_count++;
-  seq_count = seq_count % NUMBER_OF_STEPS;
+  //else if(mute_flag == false && count_in > 0)
+  //{
+  //  count_in--;
+  //}
 }
 
 ISR(TIMER1_COMPA_vect)
@@ -509,27 +403,5 @@ void set_pwm_2_3_5_6_7_8_9_10()
   TCCR2B &= ~eraser; //Clear last 3 bits
   TCCR2B |=prescaler; //Change frequency to 31KHz
 }
- 
-/*OLD CODE
-  TCCR3A = 0; //use this one for kick
-  TCCR3B = 0;
-void begin_150_timer()
-{
-  OCR3A = 3*780; //will count to 150ms
-  TCCR3B |= (1 << WGM12); //compare mode
-  TCCR3B |= (1 << CS30);// 1024 prescaler
-  TCCR3B |= (1 << CS32);
-  TIMSK3 |= (1 << OCIE3A);//enable compare interrupt
-}
-
-ISR(TIMER3_COMPA_vect)
-{
-  analogWrite(KICK,0); //after 150ms write kick to low
-  TCCR3A = 0;
-  TCCR3B = 0;//stop timer
-}
-*/
-
-
 
 
