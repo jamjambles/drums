51e783bc0b636471b5f44ff4cff744c6ea3afe11
jam <jam@Jamess-MacBook-Air.local>
2017-07-17 16:15:38 +1000

    mega drums working with leds

--- sequencer_drums/sequencer_drums.ino
+++ sequencer_drums/sequencer_drums.ino
@@ -1,34 +1,5 @@
 //Drum sequencer by James Wagner. For use with arduino mega2560
 
-
-#include <Wire.h>
-#include "Adafruit_Trellis.h"
-
-
-Adafruit_Trellis matrix0 = Adafruit_Trellis();
-Adafruit_Trellis matrix1 = Adafruit_Trellis();
-Adafruit_Trellis matrix2 = Adafruit_Trellis();
-Adafruit_Trellis matrix3 = Adafruit_Trellis();
-Adafruit_Trellis matrix4 = Adafruit_Trellis();
-Adafruit_Trellis matrix5 = Adafruit_Trellis();
-Adafruit_Trellis matrix6 = Adafruit_Trellis();
-Adafruit_Trellis matrix7 = Adafruit_Trellis();
-Adafruit_TrellisSet trellis =  Adafruit_TrellisSet( &matrix0, &matrix1, &matrix2, &matrix3,
-                                                    &matrix4, &matrix5, &matrix6, &matrix7);
-
-// set to however many you're working with here, up to 8
-#define NUMTRELLIS 8
-
-#define numKeys (NUMTRELLIS * 16)
-
-// Connect Trellis Vin to 5V and Ground to ground.
-// Connect the INT wire to pin #A2 (can change later!)
-#define INTPIN A2
-// Connect I2C SDA pin to your Arduino SDA line
-// Connect I2C SCL pin to your Arduino SCL line
-// All Trellises share the SDA, SCL and INT pin! 
-// Even 8 tiles use only 3 wires max
-
 //////// NOW THE DRUMS
 
 //sequence specs
@@ -205,17 +176,10 @@ void write_drums_high()
 //    }
 
     // if the control button is on, leave it.
-    if (!control_buttons[seq_count]) {
-      trellis.clrLED(map_seq_count_to_untz_index(seq_count-1));
-    }
-    trellis.setLED(map_seq_count_to_untz_index(seq_count));
-
+   
     seq_count++;
     seq_count = seq_count % NUMBER_OF_STEPS;
     
-    if (seq_count-1 == 0) {
-      trellis.clrLED(map_seq_count_to_untz_index(15));
-    }
   }
 
 }
@@ -313,83 +277,12 @@ void set_pwm_2_3_5_6_7_8_9_10()
 }
 
 
-// This whole thing is to map the buttons nicely to the drums,
-// without changing how the drums are addressed.
-// This function returns a negative number for the control row.
-// The first crtl button here is -1 to -16
-int get_drum_index(int i) {
-  int result = 0;
-
-  int block_num = i/16;
-  int button_num = i%16;
-
-  // flip the blocks.
-  if (block_num < 4) {
-    block_num = block_num * 2;
-  } else {
-    block_num = block_num * 2 - 7;
-  }
-
-  int row = button_num / 4 + 4*(block_num % 2);
-  int col = button_num % 4 + 4*(block_num / 2);
-
-  // this is to make the top drum start from zero
-  // if the row is -1, then it is one of the control rows. (play, pause, shift phase, etc.)
-  row--;
-  
-  result = col*7+row;
-
-//  if (row == -1) {
-//    result = -col-1;
-//  }
-  
-  // put it back together
-//  Serial.print("Row number: "); Serial.println(row);
-//  Serial.print("Col number: "); Serial.println(col);
-//  Serial.print("Block number: "); Serial.println(block_num);
-//  Serial.print("Button number: "); Serial.println(button_num);
-//  Serial.print("Drum number: "); Serial.println(result);
-
-  
-  return result;
-}
-
-int map_seq_count_to_untz_index(int seq_count) {
-  int result;
-  int block_num = seq_count / 4;
-  int block_index = seq_count % 4;
-
-  result = block_num*16 + block_index;
-  return result;
-}
-
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Trellis Demo");
-  Serial3.begin(9600);
-  // INT pin requires a pullup
-  pinMode(INTPIN, INPUT);
-  digitalWrite(INTPIN, HIGH);
-
+  //setting up the serials
+  Serial.begin(9600); //just to print to serial monitor 
+  Serial2.begin(9600); //communicate to the trellis arduino
+  Serial3.begin(9600); //receive beats 
   
-  // begin() with the addresses of each panel in order
-  // I find it easiest if the addresses are in order
-//  trellis.begin(0x70);  // only one
-   trellis.begin(0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77);  // or four!
-
-  // light up all the LEDs in order
-//  for (uint8_t i=0; i<numKeys; i++) {
-//    trellis.setLED(i);
-//    trellis.writeDisplay();    
-//    delay(2);
-//  }
-//  // then turn them off
-//  for (uint8_t i=0; i<numKeys; i++) {
-//    trellis.clrLED(i);
-//    trellis.writeDisplay();    
-//    delay(2);
-//  }
-
   // now do the drums
   seq_count = 0;
   pinMode(KICK,OUTPUT);
@@ -408,7 +301,7 @@ void setup() {
 
   // Whenever pin 19 goes from low to high write drums
   attachInterrupt(digitalPinToInterrupt(PULSE_IN),write_drums_high,RISING); 
- unmute();
+  unmute();
   
   kick_active = false;
   snare_active = false;
@@ -432,16 +325,6 @@ void setup() {
   //setting pwm frequency to 31KHz on pins 2,3,5,6,7,8,9,10
   set_pwm_2_3_5_6_7_8_9_10(); 
   
-  //Serial stuff
-  untz_poll = 0;
-
-  //initialising sequence
-  
-//  sequence = {0};
-  
-  
-  Serial.println("Setup finished");
-  Serial.println("Setting up timer.");
   begin_5_timer();
   Serial.println("Setting up the timer...");
   // The timer fucks around for 4.5 seconds before it works. 
@@ -449,67 +332,29 @@ void setup() {
   // This would probably fry the solenoid driving circuit.
   delay(4500); 
   Serial.println("Timer is good to go.");
-  usr_ctrl = true;
-
-//  for(int i = 0; i<16; i++){
-//    sequence[2+7*i] = 255;
-//  }
-  sequence[4] = 255;
-  sequence[2+7*4] = 255;
-  sequence[2+7*8] = 255;
-  sequence[2+7*12] = 255;
 
 }
 
 
 void loop() {
-  
-  delay(30); // 30ms delay is required, dont remove me!
-  int drum_index = 0;
-  if(usr_ctrl){
-    // If a button was just pressed or released...
-    if (trellis.readSwitches()) {  
-      // go through every button
-      for (uint8_t i=0; i<numKeys; i++) {
-        // if it was pressed...
-        if (trellis.justPressed(i)) {
-          
-          // Alternate the LED
-          // need to find the block number
-          drum_index = get_drum_index(i);
-          if (drum_index <= -1) {
-
-            // if the ctrl button just pressed was already on.
-            // toggle the bit at the end
-            if (control_buttons[1-drum_index]) {
-              control_buttons[1-drum_index] = false;
-            } else {
-              control_buttons[1-drum_index] = true;
-            }
-
-            
-            //I need to update the top row here
-            // fix drum index to 
-          } 
-          
-          if (trellis.isLED(i)) {
-            sequence[drum_index] = NO_HIT;
-            trellis.clrLED(i);
-          } else {
-            sequence[drum_index] = HARD;
-            trellis.setLED(i);
-          }
-        } 
-      }
-      // tell the trellis to set the LEDs we requested
-      
+  //This means there is a new coordinate 
+  while(Serial2.available() > 0){
+    char in = (char)Serial2.read();
+    int coord = (int)in;
+    if (sequence[coord]==255){
+      sequence[coord] = 0;
+    }else{
+      sequence[coord] = 255;
     }
+    //digitalWrite(SNARE,HIGH);
+    Serial.println(coord);
   }
-  trellis.writeDisplay();
-
+  
   while(Serial3.available() > 0){
     char temp = (char)Serial3.read();
     Serial3.flush();
+    Serial2.println(temp);
+    Serial2.flush();
 //    Serial.println(temp);
     switch (temp) {
       case '1':
@@ -527,10 +372,9 @@ void loop() {
       case '4':
         seq_count = 12;
         write_drums_high();
-        
         break;
       case 's':
-//        write_drums_high();
+        write_drums_high();
 //        Serial.println(temp);
         break;
       default:
